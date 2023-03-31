70faab5eceb24ced3ecc03ea6ec6a2becc03c16f
Alan Njogu <bigboydevelops@gmail.com>
2023-03-31 20:59:59 +0300

    add: get data func

--- src/database.py
+++ src/database.py
@@ -1,17 +1,18 @@
 import os
+import json
 import psycopg2
 from dotenv import load_dotenv
 
+load_dotenv()
 
-# Function to get a database connection
+
 def getConnection():
-
     conn = psycopg2.connect(
         host=os.environ['POSTGRES_HOST'],
         database=os.environ['POSTGRES_DB'],
         user=os.environ['POSTGRES_USER'],
         password=os.environ['POSTGRES_PASSWORD'],
-        port=os.environ["POSTGRES_PORT"],
+        port=os.environ["POSTGRES_PORT"]
     )
 
     return conn
@@ -22,7 +23,37 @@ def getConnection():
 # TODO: Add function to update user data
 # TODO: Add function to delete user data
 # TODO: Add function to retrieve PSV data
+def getData():
+    conn = getConnection()
+    cur = conn.cursor()
+
+    try:
+        # Execute a SELECT statement
+        cur.execute("SELECT * FROM psv")
+
+        # Fetch the rows as a list of tuples
+        rows = cur.fetchall()
+
+        # Convert the rows to a list of dictionaries
+        results = []
+        for row in rows:
+            result = {}
+            for i, col in enumerate(cur.description):
+                result[col.name] = row[i]
+            results.append(result)
+
+        # Close the database connection and cursor
+        cur.close()
+        conn.close()
+
+        return results
+    except Exception as err:
+        print(f"Error! {err}")
+
 # TODO: Add function to add PSV data
+def addData():
+    ...
+    
 # TODO: Add function to update PSV data
 # TODO: Add function to delete PSV data
 # TODO: Add function to retrieve PSV Ownwer data
